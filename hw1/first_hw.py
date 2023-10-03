5cd47ebcba89d37d6d77d103a3fc10880e133945
arinapruzhinina <pruzininaa@mail.ru>
2023-10-03 16:51:12 +0300

    add first homework

---
+++ hw1/first_hw.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def yandex_chart(url):
+
+    r = requests.get(url)
+    page = BeautifulSoup(r.text, 'lxml')
+    singer = page.findAll('div', attrs = {'class': 'd-track__meta'})
+    track = page.findAll('div', attrs = {'class' : 'd-track__name'})
+    result = {i + 1 : {singer[i].text : ' '.join(track[i].text.split())} for i in range(len(track))}
+
+    with open ('chart.json', 'w', encoding = 'utf-8') as f:
+        json.dump(result, f, ensure_ascii=False)
+
+yandex_chart('https://music.yandex.ru/chart')
